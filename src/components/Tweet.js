78451e53d447fa116c46d8acdafa334b38cb401d
Euntae Kim <hurima90@gmail.com>
2021-07-07 23:39:22 +0900

    Add upload, delete function for file(image)

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,4 +1,4 @@
-import { dbService } from 'fBase'
+import { dbService, storageService } from 'fBase'
 import React, { useState } from 'react'
 
 const Tweet = ({ tweetObj, isOwner }) => {
@@ -9,6 +9,7 @@ const Tweet = ({ tweetObj, isOwner }) => {
         if(ok){
             // delete tweet
             await dbService.doc(`tweets/${tweetObj.id}`).delete();
+            await storageService.refFromURL(tweetObj.attachmentUrl).delete();
         }
     }
     const toggleEditing = () => setEditing((prev) => !prev)
@@ -42,6 +43,7 @@ const Tweet = ({ tweetObj, isOwner }) => {
                 ) : ( 
                 <>
                     <h4>{tweetObj.text}</h4>
+                    {tweetObj.attachmentUrl && <img src={tweetObj.attachmentUrl} width="50px" height="50px" />}
                     {isOwner && (
                         <>
                             <button onClick={onDeleteClick}>Delete Tweet</button>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,14 @@
 import { dbService } from 'fBase';
+import { v4 as uuidv4 } from 'uuid'
 import React, { useState, useEffect } from 'react'
 import Tweet from 'components/Tweet';
+import { storageService } from 'fBase';
 
 // function compoenet
 const Home = ({ userObj }) => {
     const [tweet, setTweet] = useState("")
     const [tweets, setTweets] = useState([])
+    const [attachment, setAttachment] = useState()
 
     useEffect(() => {
         dbService.collection("tweets").onSnapshot((snapshot) => {
@@ -17,14 +20,23 @@ const Home = ({ userObj }) => {
         })
     }, [])
 
-    const onSubmit = (event) => {
+    const onSubmit = async(event) => {
         event.preventDefault();
-        dbService.collection("tweets").add({
+        let attachmentUrl = "";
+        if(attachment !== ""){
+            const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`)
+            const response = await attachmentRef.putString(attachment, "data_url")
+            attachmentUrl = await response.ref.getDownloadURL()
+        }
+        const tweetObj = {
             text: tweet,
             createdAt: Date.now(),
             creatorId: userObj.uid,
-        })
-        setTweet("");
+            attachmentUrl,
+        }
+        await dbService.collection("tweets").add(tweetObj);
+        setTweet("")
+        setAttachment("")
     }
 
     const onChange = (event) => {
@@ -32,11 +44,31 @@ const Home = ({ userObj }) => {
         setTweet(value);
     }
 
+    const onFileChange = (event) => {
+        const {target: {files}} = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {currentTarget: { result }} = finishedEvent;
+            setAttachment(result);
+        }
+        reader.readAsDataURL(theFile);
+    }
+
+    const onClearAttachment = () => setAttachment(null)
+
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input value={tweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Tweet" />
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px" />
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div>
+                )}
             </form>
             <div>
                 {tweets.map((tweet) => (
